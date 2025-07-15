57af6ca8d4962929cc75f4d301767db69f8c92cf
PHUC-A2 <phucban297@gmail.com>
2025-07-15 15:32:30 +0700

    #19 DOM event

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import {useDispatch, useSelector} from 'react-redux';
-import {increaseCounter, decreaseCounter} from './redux/action/counterAction';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from "./components/MyComponent";
 
-const App = () => {
-    const count = useSelector(state => state.counter.count);
-    const dispatch = useDispatch();
+// Chuyển sang class component
+import React from 'react';
 
-    return (
-        <div>
-            Hello World
-            {/*Gọi đến component*/}
-            <MyComponent>
-            </MyComponent>
-        </div>
-        // <div className="App">
-        //     <header className="App-header">
-        //         <img src={logo} className="App-logo" alt="logo"/>
-        //         <p>
-        //             Hello world Hoi Dan IT demo Redux Toolkit
-        //         </p>
-        //         <div>Count = {count}</div>
-        //         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        //         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-        //     </header>
-        // </div>
-    );
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <MyComponent>
+            
+                </MyComponent>
+            </div>
+        );
+    }
 }
 
+
+// const App = () => {
+//     const count = useSelector(state => state.counter.count);
+//     const dispatch = useDispatch();
+//
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo"/>
+//                 <p>
+//                     Hello world Hoi Dan IT demo Redux Toolkit
+//                 </p>
+//                 <div>Count = {count}</div>
+//                 <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//                 <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//             </header>
+//         </div>
+//     );
+// }
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,14 +5,38 @@ import React from 'react';
 // class component
 
 class MyComponent extends React.Component {
-// hàm render
-//     Cú pháp jsx cho phép viết code trong html
+    // hàm render
+    // Cú pháp jsx cho phép viết code trong html
+    // state là 1 biến obj
+    //  khái báo state và dùng this để gọi đến thuộc tính
+    state = {
+        name: 'Eric',
+        address: 'Hoi Dan IT',
+        age: 26
+    };
+
+    // event dùng để cung cấp các thông tin ....
+    handleClick(event){ // dùng hàm mũi tên mới tham chiếu được với this
+        console.log("Click me my button");
+        //  in state
+        console.log("My name is: ", this.state.name);
+    }
+
+    // chuột
+    hanldleOnMoverOver(event) {
+        console.log(event.pageX);
+    }
     render() {
         return (
             <div>
-                My first component
-                {/*Test gọi hàm random*/}
-                {Math.random()}
+                My name is {this.state.name} and I'm from {this.state.address}
+                {/* Ví dụ về Event */}
+                <div>
+                    <hr />
+                </div>
+                {/* <button onMouseOver={this.hanldleOnMoverOver}>Hover me</button> */}
+                {/* <button onClick={this.handleClick}>Click me</button> dùng với hàm mũi tên trên */}
+                <button onClick={(event) => { this.handleClick(event) }}>Click me</button> {/* dùng với hàm mũi trực tiếp*/}
             </div>
         );
     }
